4818a7c2daba611a579d476235037ed92c6cbf5a
deepinuniverse <deepinuniverse@gmail.com>
2022-07-14 21:27:32 +0300

    APIs

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\UserRequest;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use DB;
 
 class APIController extends Controller
 {
@@ -55,4 +56,123 @@ class APIController extends Controller
             return response()->json($error, 200);
         }
     }
+
+    // Get All Brands
+    public function getAllBrands(){
+	    try
+        {		
+            $brands = DB::table('brands')           
+            ->orderBy('created_at', 'desc')                
+            ->get();
+
+		    $response = [
+			    'code'=>200,
+			    'status'=>'success',
+			    'data'=>$brands,
+
+                ];
+		    return response()->json($response,200);
+        } catch (\Exception $e) {
+            $response = [
+                'code'=>500,
+                'message'=>$e->getMessage(),
+                'status'=>'failed'
+            ];
+            return response()->json($response,200);
+        }
+    }
+
+        // Get All Product Categories
+        public function getAllCategories()
+        {
+            try
+            {
+                                
+                $proCat = DB::table('categories')           
+                ->orderBy('created_at', 'desc')                
+                ->get();
+
+                $response = [
+                    'code'=>200,
+                    'status'=>'success',
+                    'data'=>$proCat,
+    
+                    ];
+                return response()->json($response,200);
+            } catch (\Exception $e) {
+                $response = [
+                    'code'=>500,
+                    'message'=>$e->getMessage(),
+                    'status'=>'failed'
+                ];
+                return response()->json($response,200);
+            }
+        }   
+
+
+                // Get All Product Categories
+        public function getProdcuts()
+        {
+            try
+            {     
+                $prodcuts = DB::table('products AS pt')
+                ->Join('categories As ct', 'pt.category', '=', 'ct.id')                    
+                ->Join('brands As br', 'pt.brand', '=', 'br.id')                        
+                ->select('pt.id as id','pt.uid','pt.avatar','pt.images','pt.name','pt.description','pt.stock','pt.brand as brandId','br.name as brandName','pt.category as catgoryId','ct.name as CategoryName','pt.price','pt.color', 'pt.size','pt.show','pt.created_at')
+                ->orderBy('created_at', 'desc')     
+                ->take(10)    // top 10                          
+                ->get();
+
+                $response = [
+                    'code'=>200,
+                    'status'=>'success',
+                    'data'=>$prodcuts,
+    
+                    ];
+                return response()->json($response,200);
+            } catch (\Exception $e) {
+                $response = [
+                    'code'=>500,
+                    'message'=>$e->getMessage(),
+                    'status'=>'failed'
+                ];
+                return response()->json($response,200);
+            }
+        }        
+        
+        
+        //Get Products by Vendor ID
+        public function getProductsByVenodrID(Request $request)
+        {
+            try
+            {
+                $response = [];
+                $vendorID = $request['uid'];              
+
+                $prod = DB::table('products AS pt')
+                ->Join('categories As ct', 'pt.category', '=', 'ct.id')                    
+                ->Join('brands As br', 'pt.brand', '=', 'br.id')
+                ->select('pt.id as id','pt.uid','pt.avatar','pt.images','pt.name','pt.description','pt.stock','pt.brand as brandId','br.name as brandName','pt.category as catgoryId','ct.name as CategoryName','pt.price','pt.color', 'pt.size','pt.show','pt.created_at')
+                ->where('pt.uid', $vendorID)
+                ->get();
+
+                $response = [
+                    'code'=>200,
+                    'status'=>'success',
+                    'data'=>$prod,
+                ];
+                return response()->json($response,200);
+            }
+            catch (\Exception $e) 
+            {
+                $response = [
+                    'code'=>500,
+                    'message'=>$e->getMessage(),
+                    'status'=>'failed'
+                ];
+                return response()->json($response,200);
+            }
+        }
+       
+
 }
--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/login','App\Http\Controllers\APIController@login');
 Route::post('/register','App\Http\Controllers\APIController@register');
+
+
+Route::get('/getAllBrands','App\Http\Controllers\APIController@getAllBrands');
+Route::get('/getAllCategories','App\Http\Controllers\APIController@getAllCategories');
+
+Route::get('/getProdcuts','App\Http\Controllers\APIController@getProdcuts');
+
+Route::get('/getProductsByVenodrID/{uid}','App\Http\Controllers\APIController@getProductsByVenodrID');
